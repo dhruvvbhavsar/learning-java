36a5835c717ed63d7f757dad9dab1476505aea4d
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2025-05-24 17:52:34 +0530

    encapsulation and access modifiers

--- week-2/Person.java
+++ week-2/Person.java
@@ -10,4 +10,29 @@ public class Person {
     public void greet() {
         System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        if (name.length() > 0) {
+            this.name = name;
+        } else {
+            System.out.println("Name is invalid");
+        }
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Age is invalid");
+        }
+    }
+    
 }
